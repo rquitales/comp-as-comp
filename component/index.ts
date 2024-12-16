b1ef4a7f90ddcb63f2fb78ea937c45b42593a7fa
Mikhail Shilkov <github@mikhail.io>
2024-12-16 19:33:53 +0100

    Clear

--- component/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2016-2024, Pulumi Corporation.
-
-import { componentProviderHost } from "./lib/provider";
-
-componentProviderHost();
--- program-ts/index.ts
+++
@@ -1,9 +0,0 @@
-import * as tls from "tls-self-signed-cert";
-
-const cert = new tls.SelfSignedCertificate("mycert", {
-    subject: {},
-    dnsName: "example.com",
-    validityPeriodHours: 24,
-    localValidityPeriodHours: 24,
-});
-export const certPem = cert.pem;
