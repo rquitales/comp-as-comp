f9bda68e7cd5a46e8d04f2baaf80c81ad0932fc6
Mikhail Shilkov <github@mikhail.io>
2025-01-09 13:54:37 +0000

    Add a lambda-py example

--- apps/lambda-python/__main__.py
+++ apps/lambda-python/__main__.py
@@ -2,3 +2,9 @@
 
 import pulumi_platform as platform
 
+mytable = platform.DynamoTable("table")
+
+platform.LambdaHttpApp("app", 
+    app_folder=".",
+    entry_point="index.handler",
+    dynamo_table_arn=mytable.arn)
