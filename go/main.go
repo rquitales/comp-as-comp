0cd5a6e567c4cfaf63d905b7315e94cdb49d24ef
Mikhail Shilkov <github@mikhail.io>
2025-01-15 12:59:35 +0000

    Add node metadata to Go

--- go/main.go
+++ go/main.go
@@ -3,12 +3,23 @@ package main
 import (
 	p "github.com/pulumi/pulumi-go-provider"
 	"github.com/pulumi/pulumi-go-provider/infer"
+	"github.com/pulumi/pulumi-go-provider/middleware/schema"
 )
 
 func Provider() p.Provider {
 	return infer.Provider(infer.Options{
 		Components: []infer.InferredComponent{
 			infer.Component[*RandomComponent, RandomComponentArgs, *RandomComponentState](),
+		},
+		Metadata: schema.Metadata{
+			LanguageMap: map[string]any{
+				"nodejs": map[string]any{
+					"dependencies": map[string]any{
+						"@pulumi/random": "^4.16.8",
+					},
+					"respectSchemaVersion": true,
+				},
+			},
 		},
 	})
 }
