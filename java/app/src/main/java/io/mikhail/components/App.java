24053392e52fe355bc5d70acad608d268ad198d8
Mikhail Shilkov <github@mikhail.io>
2025-03-27 13:11:04 +0100

    Update Java

--- java/app/src/main/java/io/mikhail/components/App.java
+++ java/app/src/main/java/io/mikhail/components/App.java
@@ -6,6 +6,6 @@ import com.pulumi.provider.internal.ComponentProviderHost;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
-		new ComponentProviderHost(new Metadata("java-components"), App.class.getPackage()).start(args);
+		new ComponentProviderHost("java-components", App.class.getPackage()).start(args);
     }
 }
